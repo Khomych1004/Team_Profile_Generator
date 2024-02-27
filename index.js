8f0a97f2957f7a6ca1cfce3c33c12c99c2cf5d0e
Khomych1004 <khomych100488@gmail.com>
2024-02-27 22:26:59 +0000

    added function to launch and create Manager

--- index.js
+++ index.js
@@ -10,6 +10,64 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// Questions to create a Manager
+const addManager = () => {
+    return inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'manager name',
+            validate: input => {
+                if (!input) {
+                    return 'cannot be empty';
+                }
+                return true;
+            }
+        },
+        {
+            name: 'employeeId',
+            type: 'input',
+            message: 'employee ID',
+            validate: input => {
+                if (!input) {
+                    return 'cannot be empty';
+                }
+                return true;
+            }
+        },
+        {
+            name: 'emailAddress',
+            type: 'input',
+            message: 'Email address',
+            validate: input => {
+                if (!input) {
+                    return 'cannot be empty';
+                }
+                return true;
+            }
+        },
+        {
+            name: 'officeNumber',
+            type: 'input',
+            message: 'Office number',
+            validate: input => {
+                if (!input) {
+                    return 'cannot be empty';
+                }
+                return true;
+            }
+        }
+    ]).then(answers => new Manager(
+        answers.name,
+        answers.employeeId,
+        answers.emailAddress,
+        answers.officeNumber
+    ));
+};
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Start function
+function init() {
+    addManager().then(manager => {console.log("Was created", manager)});
+};
 
+init();
